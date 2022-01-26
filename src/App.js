7c239e047af0eb382d4c88ba89aacbfbf156a4aa
jairo cogollo <mickt681@gmail.com>
2022-01-26 02:19:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import './styles.css';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import Home from './pages/home.js';
+// import About from './pages/about.js';
+// import Services from './pages/services.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div class="container-header"></div>
+      
+
+      {/* <div class="container-scroll">      </div> */}
+      <br></br>
+      <h1 class ="title">Corporación sociedad prosperar</h1>
+      <br></br>
+      <br></br>
+      <div class="container-scroll">
+        <h3>aqui van las imagenes </h3>
+      </div>
+      <br></br>
+      
+
+      <div class="container-noticias">
+        <h1>Noticias</h1>
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div class="footer">
+
+        <h1>Corporación sociedad prosperar</h1>
+        <h2>Sede principal</h2>
+        <h3>Direccion</h3>
+        <h3>codigo postal</h3>
+      </div>
+
     </div>
+
   );
 }
 
---
+++ src/pages/services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css'
+
+const Services = () => {
+  return (
+    <div class= "container">
+      <h1>Services</h1>
+    </div>
+  );
+};
+
+export default Services;
